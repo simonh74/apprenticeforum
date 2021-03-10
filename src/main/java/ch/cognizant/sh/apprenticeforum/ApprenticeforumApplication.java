c8813ab88b2b039c9e114d6c0787515c93de5bd7
simonh74 <simon_h@gmx.ch>
2021-03-10 16:18:41 +0100

    initial commit - created an empty project and added dependencies in the pom.xml file

---
+++ src/main/java/ch/cognizant/sh/apprenticeforum/ApprenticeforumApplication.java
@@ -0,0 +1,12 @@
+package ch.cognizant.sh.apprenticeforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprenticeforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApprenticeforumApplication.class, args);
+	}
+}
---
+++ src/test/java/ch/cognizant/sh/apprenticeforum/ApprenticeforumApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.cognizant.sh.apprenticeforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprenticeforumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
